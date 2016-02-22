7f1f35d959499514814dff150a0145cfc27afd62
Audeline ZALI <Audeline ZALI>
2016-02-22 17:15:32 -0400

    edition des fichiers sql

---
+++ db/database.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE ALPHA_PONG; 
+use ALPHA_PONG;
+
+grant all privileges on ALPHA_PONG.*to 'pong_user'@'localhost' identified by 'pong.mdp';
---
+++ db/structure.sql
@@ -0,0 +1,8 @@
+CREATE TABLE JOUEUR
+(
+	njoueur int (2) not null PRIMARY KEY auto_increment,
+	pseudo_j varchar(50) not null,
+	mdp varchar(20) not null,
+	nbre_victoire int(2),
+	nbre_defaite int(2)
+)engine=innodb character set utf8 collate utf8_unicode_ci;

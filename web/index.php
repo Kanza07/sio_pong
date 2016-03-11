7f32dc52a95f5ba52217b81b354a3cf256a61dab
Kanza07 <marvin.rosillette@kanopta.com>
2016-03-11 10:27:04 -0400

    reorganisation des fichiers

---
+++ web/index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<title>Pong | Accueil</title>
+		<link href="css/styles.css" rel="stylesheet"/>
+		<link href="css/responsive.css" rel="stylesheet">
+	</head>
+	<body>
+
+		<div>
+			<a><img src="img/pngs/Calque6.png"/></a>
+		</div>
+
+		<div>
+			<a href="#2"><img src="img/pngs/regles.png" id="alpha"/></a>
+		</div>
+
+		<div>
+		  	<a href="#3"><img src="img/pngs/score.png" id="beta"/></a>
+		</div>
+
+		<div>
+		  	<a href="../views/loading.php"><img src="img/pngs/play.png" id="teta"/></a>
+		</div>
+
+		<!---->
+		<p>
+		   <input type="checkbox" id="test2" checked="checked">
+		   <label for="test2">Musique</label>
+		</p>
+		<!---->
+
+	</body>
+</html>
